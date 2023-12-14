bbc0544b53dc4429782eef246292d3cb8a1499dc
Jordy <jordycorporan95@gmail.com>
2023-12-13 21:16:26 -0500

    add user schema

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  provider: {
+    type: String,
+  },
+  provider_id: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  provider_pic: {
+    type: String,
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  timestamps: true,
+});
+
+UserSchema.methods.follow = function (user_id) {
+  if (this.following.indexOf(user_id) === -1) {
+    this.following.push(user_id);
+  }
+  return this.save();
+};
+
+UserSchema.methods.addFollower = function (fs) {
+  this.followers.push(fs);
+};
+
+module.exports = mongoose.model("User", UserSchema);
