c68f8bee55613d316301fa5b32f64ad22109ca9f
Jordy <jordycorporan95@gmail.com>
2023-12-14 22:19:53 -0500

    add new word

--- server/models/User.js
+++ server/models/User.js
@@ -44,8 +44,8 @@ UserSchema.methods.follow = function (user_id) {
   return this.save();
 };
 
-UserSchema.methods.addFollower = function (follower) {
-  this.followers.push(follower);
+UserSchema.methods.addFollower = function (fs) {
+  this.followers.push(fs);
 };
 
 module.exports = mongoose.model("User", UserSchema);
